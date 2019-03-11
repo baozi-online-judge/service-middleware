837a535ba849bd4f42c4472d5af8b04d60d14640
HeskeyBaozi <hezhiyu233@foxmail.com>
2019-03-12 00:06:59 +0800

    fix enum

--- app/graphql/common/enums/role.js
+++
@@ -1,9 +0,0 @@
-const { GraphQLEnumType } = require('graphql');
-
-module.exports = new GraphQLEnumType({
-  name: 'Role',
-  values: {
-    Admin: { value: 1 },
-    Regular: { value: 2 }
-  }
-});
--- app/graphql/common/resolver.js
+++ app/graphql/common/resolver.js
@@ -1,3 +1,25 @@
+const { GraphQLEnumType, GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
+
 module.exports = {
-  Date: require('./sclars/date')
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    parseValue(value) {
+      return new Date(value);
+    },
+    serialize(value) {
+      return value.getTime();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return Number.parseInt(ast.value, 10);
+      }
+      return null;
+    }
+  }),
+  Role: {
+    ADMIN: 1,
+    REGULAR: 2
+  }
 };
